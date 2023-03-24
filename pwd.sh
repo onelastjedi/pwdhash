834fcd977b4b837f84c63ffb81de9d39e021cc21
J.D <jd@phon.one>
2023-03-24 16:45:30 -0300

    Add md5 hmac base64 encoding of given password

--- pwd.sh
+++ pwd.sh
@@ -4,6 +4,9 @@
 # combination of page URI
 # and plaintext password
 
+PREFIX='@@'
+RE='[^a-zA-Z\d\s:]'
+
 # Get user input and 
 # save to URI, PLAIN
 
@@ -11,5 +14,19 @@ read -p "Domain: " URI
 echo "Password: "
 read -s PLAIN
 
-# Use openssl and some utility
-# functions to get hashed password
+# Use openssl to get hashed password
+# and save to BASE64
+
+BASE64=$(echo -n $URI | openssl md5 -hmac $PLAIN -binary | openssl enc -base64 -A)
+
+# Set hashed password length
+# based on plain and prefix lengths
+
+SIZE=$((${#PREFIX} + ${#PLAIN}))
+
+if [[ $PLAIN =~ $RE ]]
+then
+  NONALPHA=true
+else 
+  NONALPHA=false
+fi
