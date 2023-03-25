c231db023055c7f05eac7715000bf91b9342c290
J.D <jd@phon.one>
2023-03-24 22:53:05 -0300

    Add `apply_constraint` function

--- pwdha.sh
+++ pwdha.sh
@@ -15,9 +15,9 @@ echo "Password: "
 read -s PLAIN
 
 # Use openssl to get hashed password
-# and save to BASE64
+# and save to DIGEST
 
-BASE64=$(echo -n $URI | openssl md5 -hmac $PLAIN -binary | openssl enc -base64 -A)
+DIGEST=$(echo -n $URI | openssl md5 -hmac $PLAIN -binary | openssl enc -base64 -A)
 
 # Set hashed password length
 # based on plain and prefix lengths
@@ -26,7 +26,36 @@ SIZE=$((${#PREFIX} + ${#PLAIN}))
 
 if [[ $PLAIN =~ $RE ]]
 then
-  NONALPHA=true
+  ALPHANUM=true
 else 
-  NONALPHA=false
+  ALPHANUM=false
 fi
+
+apply_constraints () {
+  local digest=$1
+  local size=$2
+  local alphanum=$3
+
+  local start_size=$(($SIZE - 4))
+  local result=${digest:0:$start_size}
+  local extras=${digest:$start_size}
+  echo $digest
+  echo $result
+  echo $extras
+}
+
+apply_constraints $DIGEST $SIZE $ALPHANUM
+
+# String
+text="Welcome"
+
+# Set space as the delimiter
+IFS=''
+
+# Read the split words into an array
+# based on space delimiter
+read -ra newarr <<< "$text"
+
+arr=$(echo "abcdefg" | grep -o .)
+#echo $arr
+
