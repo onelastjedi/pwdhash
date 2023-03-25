6e82d2f8fab9998e266e7d21828c3ca0e24e7fcf
J.D <jd@phon.one>
2023-03-24 22:54:06 -0300

    Add initial implementation on FreePascal

---
+++ pwdhash.pas
@@ -0,0 +1,42 @@
+program pwdhash;
+
+uses
+  RegExpr, Process;
+
+var
+  re: TRegExpr;
+  uri: string;
+  plain: string;
+  digest: ansistring;
+  prefix: string;
+  size: integer;
+  nonalpha: boolean;
+  
+begin
+  re:= TRegExpr.Create;
+  re.Expression:= '\W';
+  prefix:= '@@';
+
+  write('Domain: ');
+  readln(uri);
+  write('Password: ');
+  readln(plain);
+
+  size:= length(plain) + length(prefix);
+  nonalpha:= re.Exec(plain);
+
+  if RunCommand(
+    '/bin/bash', 
+    [
+      '-c',
+      'echo -n ' 
+      + uri 
+      + '| openssl md5 -hmac ' + plain 
+      + ' -binary | openssl enc -base64 -A'
+    ],
+    digest
+  )
+  then writeln(digest);
+  
+  writeln(size);
+end.
